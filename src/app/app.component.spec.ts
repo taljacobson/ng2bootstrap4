1340d362560cd726747cc755e2aeab9d0a372125
taljacobson <tal1989@gmail.com>
2016-10-08 19:05:04 +0300

    add routermodule test, remove appworks test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,34 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import {
+  RouterTestingModule
+} from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
 
 describe('App: Ng2bootstrap4', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes([{
+          path: 'home',
+          component: HomeComponent
+        },
+          {
+            path: 'about',
+            component: AboutComponent
+          }
+        ])
+      ]
     });
   });
 
@@ -18,16 +38,4 @@ describe('App: Ng2bootstrap4', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
 });
